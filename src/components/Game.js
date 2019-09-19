7ee78c455ce6b253fc292e2d5ff5aba4bcfecb4b
jmelfers <esjuanma@hotmail.com>
2019-09-19 00:12:31 -0300

    Client - CSS/Board-Styles

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const Cell = ({ state, onClick }) => (
-    <td className={`cell ${Object.keys(state).join(' ')}`} onClick={onClick}>
+    <td className={`cell ${Object.keys(state).join(' ')} ${state.value === 0 && 'empty'}`} onClick={onClick}>
         {state.value || ''}
     </td>
 );
 
 const Game = ({ game, onClick }) => (
-    <table className="game-container">
+    <table className={`game-container ${game.ended && 'ended'}`}>
         <tbody>
             {game.board.map((row, y) => (
                 <tr>
                     {row.map(
-                        (cell, x) => <Cell state={cell} onClick={() => onClick({x, y})} />
+                        (cell, x) => <Cell state={cell} onClick={() => onClick({ x, y })} />
                     )}
                 </tr>
             ))}
